9d5ca0547bf1026c73766a8b6e0d8c43da777203
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2022-06-06 11:55:52 -0700

    Adjust compile flags for unimplemented n-api symbols. These are going to have to be implemented at some point.

--- tools/create-node-symbols.mjs
+++ tools/create-node-symbols.mjs
@@ -28,12 +28,24 @@ const require = createRequire(self)
 // veil supports v3 of n-api
 const { v3 } = require('../deps/node-api-headers').symbols
 
+// veil is missing these apis from v3 (should be added)
+const unimplemented = new Set([
+  "napi_adjust_external_memory",
+  "napi_create_string_latin1",
+  "napi_create_string_utf16",
+  "napi_get_value_string_latin1",
+  "napi_get_value_string_utf16",
+  "napi_run_script"
+])
+
 // node_symbols.txt is just a list of symbols (one symbol per line). cmake will read this
 // and set the compiler commands for undefined symbols
 const file = resolve(fileURLToPath(self), '..', '..', 'src', 'napi', 'node_symbols.txt')
 
+const isImplemented = (value) => !unimplemented.has(value)
+
 // create the list from node-api-headers
-const symbolsList = [ ...v3.js_native_api_symbols, ...v3.node_api_symbols ].join(EOL)
+const symbolsList = [ ...v3.js_native_api_symbols, ...v3.node_api_symbols ].filter(isImplemented).join(EOL)
 
 // flush symbols to disk
 writeFileSync(file, symbolsList, 'utf8')
