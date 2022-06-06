663dc6c785bf26cef7cad7cb57496720c4f7f934
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2022-06-06 10:48:39 -0700

    Fix incorrect package naming in final artifact.

--- tools/create-package.sh
+++ tools/create-package.sh
@@ -6,28 +6,37 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 SRC_BUILD="${SCRIPT_DIR}/../build"
 SRC_PACKAGE="${SRC_BUILD}/package"
 
-VEIL_OS=${1:-"@"}
+if [ "${1}x" = "x" ]; then
+  case "$OSTYPE" in
+    darwin*)
+      VEIL_OS=macos
+    ;;
+    linux*)
+      VEIL_OS=linux
+    ;;
+    msys*)
+      VEIL_OS=windows
+    ;;
+    *)
+      VEIL_OS=unknown
+    ;;
+  esac
+else
+  VEIL_OS="${1}"
+fi
+
 VEIL_ARCH=${2:-x64}
 VEIL_VERSION="1.0.0"
 VEIL_PACKAGE_NAME="veil-${VEIL_VERSION}-${VEIL_OS}-${VEIL_ARCH}"
 VEIL_PACKAGE_PATH="${SRC_PACKAGE:?}/${VEIL_PACKAGE_NAME}"
 
-if [[ "$VEIL_OS" = "@" ]]; then
-  case "$OSTYPE" in
-    darwin*)  VEIL_OS=macos ;;
-    linux*)   VEIL_OS=linux ;;
-    msys*)    VEIL_OS=windows ;;
-    *) VEIL_OS=unknown
-  esac
-fi
-
-if [[ "${VEIL_OS}" = "macos" ]]; then
+if [ "${VEIL_OS}" = "macos" ]; then
   BUILD_OS="darwin"
 else
   BUILD_OS=${VEIL_OS}
 fi
 
-if [[ "${VEIL_ARCH}" = "x64" ]]; then
+if [ "${VEIL_ARCH}" = "x64" ]; then
   BUILD_ARCH="x86_64"
 else
   BUILD_ARCH=${VEIL_ARCH}
@@ -42,7 +51,7 @@ if [ -d "${VEIL_PACKAGE_PATH}" ]; then
 fi
 mkdir "${VEIL_PACKAGE_PATH}"
 
-if [[ "$VEIL_OS" = "windows" ]]; then
+if [ "$VEIL_OS" = "windows" ]; then
   VEIL_EXE="${SRC_BUILD}/${BUILD_ARCH}-${BUILD_OS}/release/bin/Release/veil.exe"
   VEIL_BIN_DIR="${VEIL_PACKAGE_PATH}"
 else
@@ -61,7 +70,7 @@ cp "${VEIL_EXE}" "${VEIL_BIN_DIR}"
 cp "${SCRIPT_DIR}/../LICENSE" "${VEIL_PACKAGE_PATH}/LICENSE"
 cp "${SCRIPT_DIR}/../NOTICE" "${VEIL_PACKAGE_PATH}/NOTICE"
 
-if [[ "$VEIL_OS" = "windows" ]]; then
+if [ "$VEIL_OS" = "windows" ]; then
   (cd "${SRC_PACKAGE}" && 7z a -r -tzip "${VEIL_PACKAGE_NAME}.zip" "${VEIL_PACKAGE_NAME}")
 else
   (cd "${SRC_PACKAGE}" && tar -czf "${VEIL_PACKAGE_NAME}.tgz" "${VEIL_PACKAGE_NAME}")
