6c0f6f686393737ba13049759bee39f72f533117
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2022-06-16 15:41:25 -0700

    Build Fix. Missing .c file.

--- cmake/libutf16.cmake
+++ cmake/libutf16.cmake
@@ -2,10 +2,11 @@ set(TARGET_LIB_UTF16 libutf16)
 
 add_library(${TARGET_LIB_UTF16}
     STATIC
+    ${ROOT_DIR}/deps/libutf16/src/utf8_to_utf16.c
     ${ROOT_DIR}/deps/libutf16/src/utf8_to_utf16_one.c
-    ${ROOT_DIR}/deps/libutf16/src/utf16_to_utf8_one.c
     ${ROOT_DIR}/deps/libutf16/src/utf16_to_utf8.c
-    ${ROOT_DIR}/deps/libutf16/src/utf8_cstd.c)
+    ${ROOT_DIR}/deps/libutf16/src/utf16_to_utf8_one.c
+)
 
 target_include_directories(${TARGET_LIB_UTF16}
     PUBLIC
