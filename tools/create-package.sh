1d2216f169eaa77308c3d4d0bed95ac9fd2c3ae9
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2022-07-15 14:07:24 -0700

    Package build script clean up

--- tools/create-package.sh
+++ tools/create-package.sh
@@ -3,78 +3,67 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
-SRC_BUILD="${SCRIPT_DIR}/../build"
-SRC_PACKAGE="${SRC_BUILD}/package"
+SOURCE_ROOT="${SCRIPT_DIR}/.."
 
 BUILD_TAG=$1
 PACKAGE_TAG=$2
+BUILD_TYPE=$3
 
-if [ "${1}x" = "x" ]; then
-  case "$OSTYPE" in
-    darwin*)
-      BUILD_TAG=x86_64-darwin
+if [[ "${BUILD_TAG}x" == "x" ]]; then
+  echo "create-package.sh build_tag package_tag [build_type]"
+  echo "Error: missing build_tag"
+  exit 1
+fi
+
+if [[ "${PACKAGE_TAG}" == "default" ]]; then
+  case "${BUILD_TAG}" in
+    x86_64-darwin)
       PACKAGE_TAG=macos-x64
-    ;;
-    linux*)
-      BUILD_TAG=x86_64-linux
+      ;;
+    x86_64-linux)
       PACKAGE_TAG=linux-x64
-    ;;
-    msys*)
-      BUILD_TAG=x86_64-windows
+      ;;
+    x86_64-windows)
       PACKAGE_TAG=windows-x64
-    ;;
+      ;;
     *)
-      echo "create-package.sh build_tag package_tag"
-      echo "Error: missing build_tag"
-      exit 1
-    ;;
+      echo "Error: No default for build_tag = '${BUILD_TAG}'"
+      ;;
   esac
 fi
 
-if [ "${PACKAGE_TAG}x" = "x" ]; then
-  echo "create-package.sh build_tag package_tag"
+if [[ "${PACKAGE_TAG}x" == "x" ]]; then
+  echo "create-package.sh build_tag package_tag [build_type]"
   echo "Error: missing package_tag"
   exit 1
 fi
 
-VEIL_VERSION="$(cat "${SCRIPT_DIR}/../config/version")"
-VEIL_BUILD_PATH="${SRC_BUILD:?}/${BUILD_TAG}/release"
-VEIL_PACKAGE_NAME="veil-${VEIL_VERSION}-${PACKAGE_TAG}"
-VEIL_PACKAGE_PATH="${SRC_PACKAGE:?}/${VEIL_PACKAGE_NAME}"
-
-VEIL_WIN_EXE="${VEIL_BUILD_PATH}/bin/Release/veil.exe"
-
-if [ ! -d "${SRC_PACKAGE:?}" ]; then
-  mkdir "${SRC_PACKAGE:?}"
+if [[ "${BUILD_TYPE}x" == "x" ]]; then
+  BUILD_TYPE="release"
 fi
 
-if [ -d "${VEIL_PACKAGE_PATH}" ]; then
-  rm -r "${VEIL_PACKAGE_PATH}"
-fi
-mkdir "${VEIL_PACKAGE_PATH}"
-
-if [ -f "${VEIL_WIN_EXE}" ]; then
-  VEIL_EXE="${VEIL_WIN_EXE}"
-  VEIL_BIN_DIR="${VEIL_PACKAGE_PATH}"
-else
-  VEIL_EXE="${VEIL_BUILD_PATH}/bin/veil"
-  VEIL_BIN_DIR="${VEIL_PACKAGE_PATH}/bin"
-  mkdir "${VEIL_BIN_DIR}"
-fi
-
-if [ ! -f "${VEIL_EXE}" ]; then
-  echo "no executable found in build directory: ${VEIL_EXE}"
-  rm -r "${VEIL_PACKAGE_PATH}"
+if [[ "${BUILD_TYPE}" != "release" ]] && [[ "${BUILD_TYPE}" != "debug" ]]; then
+  echo "create-package.sh build_tag package_tag [build_type]"
+  echo "Error: build_type must be 'release' or 'debug'"
   exit 1
 fi
 
-cp "${VEIL_EXE}" "${VEIL_BIN_DIR}"
-cp "${SCRIPT_DIR}/../LICENSE" "${VEIL_PACKAGE_PATH}/LICENSE"
+VEIL_PACKAGE_NAME="veil-$(cat config/version)-${PACKAGE_TAG}"
 
-if [ -f "${VEIL_WIN_EXE}" ]; then
-  (cd "${SRC_PACKAGE}" && 7z a -r -tzip "${VEIL_PACKAGE_NAME}.zip" "${VEIL_PACKAGE_NAME}")
+cd "${SOURCE_ROOT}"
+mkdir -p build/package
+
+echo "creating ${VEIL_PACKAGE_NAME}..."
+
+if [[ "${PACKAGE_TAG}" == *"windows"* ]]; then
+  VEIL_PACKAGE_FILE="build/package/${VEIL_PACKAGE_NAME}.zip"
+  rm -f "${VEIL_PACKAGE_FILE}"
+  cd "build/${BUILD_TAG}/release/bin/Release"
+  7z a -r -tzip "${SOURCE_ROOT}/${VEIL_PACKAGE_FILE}" veil.exe
 else
-  (cd "${SRC_PACKAGE}" && tar -czf "${VEIL_PACKAGE_NAME}.tgz" "${VEIL_PACKAGE_NAME}")
+  VEIL_PACKAGE_FILE="build/package/${VEIL_PACKAGE_NAME}.tgz"
+  rm -f "${VEIL_PACKAGE_FILE}"
+  tar -czf "${VEIL_PACKAGE_FILE}" -C "build/${BUILD_TAG}/release/bin" veil
 fi
 
-rm -r "${VEIL_PACKAGE_PATH}"
+echo "finished: ${VEIL_PACKAGE_FILE}"
